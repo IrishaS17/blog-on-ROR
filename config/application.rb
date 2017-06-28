063dcc93f9f7319539cb822b66c35bf250241012
IrishaS17 <irishasidorenko17@gmail.com>
2017-06-28 15:32:05 +0300

    Add locale

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,11 @@ module A
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    I18n.available_locales = [:en, :by, :ru]
+    config.time_zone = 'Minsk'
+    config.i18n.default_locale = :en
+    def default_url_options(options = {}) 
+    	{ locale: I18n.locale }.merge options 
+    end
   end
 end
