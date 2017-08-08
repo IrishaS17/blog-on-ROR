29501f07f909874ab4f30f1010b1d0b8f90230cb
IrishaS17 <irishasidorenko17@gmail.com>
2017-08-08 15:13:44 +0300

    fix errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,36 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_post, only: :show
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
  
   def index
     @posts = Post.paginate(:page => params[:page], per_page: 5)
   end
 
   def show
+  end
+  
+  def create
+    @post = Post.new(post_params) #стронг параметрс - фильтрация параметров, которые мы получаем
+    if @post.save
+      redirect_to  @post #если сохранилось перенаправляем на созданный пост
+    else
+      render :new # вводит ещё раз
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      redirect_to  @post
+    else
+      render :edit
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope "/:locale" do
+    devise_for :users
     root 'posts#index'
-    resources :posts, only: [:show, :index]
+    resources :posts, only: [:show, :index, :create, :edit]
     resources :users
     resources :tags, only: [:show]
     resources :categories, only: [:show] 
 
+
     namespace :admin do
-      resources :posts, except: [:show, :index]
+      resources :posts, except: [:show, :index, :create, :edit]
       resources :categories, except: [:show]
       resources :pictures, only: [:create, :destroy]
 
