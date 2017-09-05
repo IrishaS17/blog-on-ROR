78f2716c1e55b051a629be328b379e6117ebbbfa
IrishaS17 <irishasidorenko17@gmail.com>
2017-09-05 17:53:32 +0300

    ....

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,14 +55,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.default_options = { from: 'support@irishblog.com' }
-    config.action_mailer.default_url_options = { :host => 'https://irish-blog.herokuapp.com/en/' }
+    config.action_mailer.default_options = { from: '373601logist@gmail.com' }
+    config.action_mailer.default_url_options = { :host => 'https://irish-blog.herokuapp.com/' }
     config.action_mailer.smtp_settings = {
-          :address        => 'smtp.gmail.com',
-          :domain         => 'irish-blog.herokuapp.com',
+         :address        => 'smtp.gmail.com',
+          :domain         => 'gmail.com',
           :port           => 587,
-          :user_name      => 'irishaapple@gmail.com',
-          :password       => 'lovemeandfuckyou',
+          :user_name      => '373601logist@gmail.com',
+          :password       => "373601373601",
           :authentication => :plain,
           :enable_starttls_auto => true
     }
