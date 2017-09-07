17b92eecf31f8c8f11d8fa37739c10b4ca5e7700
IrishaS17 <irishasidorenko17@gmail.com>
2017-09-07 19:37:04 +0300

    Add email verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :name, presence: true, uniqueness: true
+  validates :email, :presence => true, :email => true
+  #validates_format_of :email,:with => Devise::email_regexp
   #validates :sex, presence: true
   #mount_uploader :image, ImageUploader
   devise :database_authenticatable, :registerable,
